fbee685c63bb1c72f41287f09ed64b82835ac2f2
leonredman <leonproart@yahoo.com>
2021-03-01 01:22:29 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 //import LoginForm from './components/LoginForm';
 //import DemoLogin from './components/DemoLogin';
 //import SideMenu from './components/SideMenu';
 import Header from './components/Header';
 import AccountRegister from './components/AccountRegister';
+import TicketForm from './components/TicketForm';
 import Dashboard from './components/Dashboard';
+import MainLogin from './components/MainLogin';
+import ResetPassword from './components/ResetPassword';
+import WorkTicket from './components/WorkTicket';
+
 
 
 const tracker = () => { //this fixes error
 //export default () => {
     return(
         <div>
+           
             <div>
+                <BrowserRouter>
                 <Header />
-            </div>
+                <Route path="/" exact component={ MainLogin } />
+                <Route path="/Dashboard" component={ Dashboard } />
+                <Route path="/TicketForm" exact component={ TicketForm } />
+                <Route path="/AccountRegister" exact component={ AccountRegister } />
+                <Route path="/ResetPassword" exact component={ ResetPassword } />
+                <Route path="/WorkTicket" exact component={ WorkTicket } />
 
-            <div className="ui grid">
-                <div className="ui row">
-                    
+
+                </BrowserRouter>
+            </div>
+         
                     {/* <div className="three wide column">
                         <SideMenu />
                     </div> */}
@@ -34,15 +48,17 @@ const tracker = () => { //this fixes error
                                     <AccountRegister />
                                 </div> */}
 
-                                <div className="sixteen wide column">
-                                    <Dashboard />
-                                </div>
-                                
-                </div>
-            </div>
-           
+                                {/* <div className="six wide centered column">
+                                    <TicketForm />
+                                </div> */}
 
-           
+                                {/* <div className="sixteen wide column">
+                                    <Dashboard />
+                                </div> */}
+                                
+                {/* </div>
+            </div> */}
+        
         </div>
    );
 };
