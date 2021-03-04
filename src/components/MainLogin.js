e957e08856d157f62af7c1638ed30a36522f63c7
leonredman <leonproart@yahoo.com>
2021-03-03 23:32:40 -0700

    component props

--- src/components/MainLogin.js
+++ src/components/MainLogin.js
@@ -1,58 +1,20 @@
 import React from 'react';
+import LoginForm from './LoginForm';
+import DemoLogin from './DemoLogin';
 
 const MainLogin = () => {
     return ( 
         <div className="mainLoginContainer">
             <div className="ui grid">
 
-            <div className="six wide centered column">
-       <div className="loginContainer">
-            <h1>Log In</h1>
-            <form className="ui form">
-                <div className="field">
-                <label>
-                    Email:
-                    <input type="email" name="email" />
-                </label>
+                <div className="six wide centered column">
+                    <LoginForm />
                 </div>
-                  <br />
-                    <div className="field">
-                        <label>
-                            Password:
-                            <input type="text" name="password" />
-                        </label>
-                    </div>
-                        <br />
-                            <input type="checkbox" name="remember" /> Remember me
-                                <br />
-                                <br />
-                                    <input type="submit" value="Submit" />
-            </form>
-            <br />
-                <div>
-                    <h3>Register as a new User</h3>
+
+                <div className="four wide centered column">
+                    <DemoLogin />
                 </div>
-                <br />
-                <div>
-                    <h3>Forgot your Password?</h3>
-                </div>
-        </div>
-        </div>
-<div className="four wide centered column">
 
-        <div className="DemoLoginContainer" style={{marginTop: '60px', display: 'flex', flexDirection: 'column'}}>
-            <h1>Demo Log In</h1>
-
-        <button className="ui button" style={{margin: '6px'}}>Admin Login</button>
-        <button className="ui button" style={{margin: '6px'}}>Manager Login</button>
-        <button className="ui button" style={{margin: '6px'}}>Developer Login</button>
-        <button className="ui button" style={{margin: '6px'}}>Quality Control Login</button>
-
-        <p>All newly registered users are assigned 
-        the role of Submitter. You can demo other roles for 
-        this application without registering.</p>
-        </div>
-        </div>
             </div>   
         </div>
     );
