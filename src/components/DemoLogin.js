0f74f289b6c03a4d019febcc7e6ae44230cb82b3
leonredman <leonproart@yahoo.com>
2021-03-03 23:30:53 -0700

    component props

--- src/components/DemoLogin.js
+++ src/components/DemoLogin.js
@@ -1,19 +1,21 @@
 import React from'react';
+import LogButton from './LogButton';
 
 const DemoLogin = () => {
     return (
-
+        <div className="demoLogContainer">
         <div className="DemoLoginContainer" style={{marginTop: '60px', display: 'flex', flexDirection: 'column'}}>
             <h1>Demo Log In</h1>
 
-        <button className="ui button" style={{margin: '6px'}}>Admin Login</button>
-        <button className="ui button" style={{margin: '6px'}}>Manager Login</button>
-        <button className="ui button" style={{margin: '6px'}}>Developer Login</button>
-        <button className="ui button" style={{margin: '6px'}}>Quality Control Login</button>
+        <LogButton text="Admins. Login" />
+        <LogButton text="Manager Login" />
+        <LogButton text="Developer Login" />
+        <LogButton text="QA-Agent Login" /> 
 
         <p>All newly registered users are assigned 
         the role of Submitter. You can demo other roles for 
         this application without registering.</p>
+        </div>
         </div>
     );
 };
