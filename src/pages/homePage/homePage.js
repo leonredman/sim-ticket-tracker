6d0e58e11957228b858a17187630c8ffd6578b22
leonredman <leonproart@yahoo.com>
2021-09-17 19:03:15 -0700

    layout rows adjustment

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -23,8 +23,8 @@ const HomePage = () => {
       </div>
       <div className="ui vertical stripe segment">
         <div className="ui middle aligned stackable grid container">
-          <div className="row">
-            <div className="sixteen wide  column">
+          <div className="centered row">
+            <div className="six wide centered column">
               <div className="ui text container">
                 <h1>Stay On Track To Hit Your Goals</h1>
               </div>
@@ -40,8 +40,61 @@ const HomePage = () => {
               />
             </div>
 
-            <div className="eight wideright floated column">
+            <div className="eight wide right floated column">
               <h2 className="ui header">A Management Tool thats Easy To Use</h2>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="ui inverted vertical stripe segment">
+        <div className="ui middle aligned stackable grid container">
+          <div className="row">
+            <div className="eight wide column">
+              <h3 className="header">Features - The Big Picture</h3>
+              <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum."
+              </p>
+            </div>
+
+            <div className="eight wide floated right column">
+              <img
+                className="ui medium image"
+                src="https://semantic-ui.com/images/wireframe/image.png"
+                alt="placeholder"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ui vertical stripe segment">
+        <div className="ui middle aligned stackable grid container">
+          <div className="row">
+            <div className="eight wide column">
+              <img
+                className="ui medium image"
+                src="https://semantic-ui.com/images/wireframe/image.png"
+                alt="placeholder"
+              />
+            </div>
+
+            <div className="eight wide right floated column">
+              <h2 className="ui header">User Management</h2>
               <p>
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
