dc08ec773ab2f0a41d373d9517138e971f709274
leonredman <leonproart@yahoo.com>
2021-10-09 18:02:12 -0700

    dropdown fields edits

--- src/pages/updateForm/UpdateForm.js
+++ src/pages/updateForm/UpdateForm.js
@@ -80,219 +80,224 @@ const TicketForm = (props) => {
           </div>
         </div>
         <div className="ui row"></div>
+
         <div className="six wide centered column">
-          <div className="ui form">
-            <div className="fields">
-              <div className="six wide field">
-                <TextField
-                  label="Customer Name"
-                  value={values.customerName}
-                  onChange={(customerName) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      customerName,
-                    }))
-                  }
-                />
-              </div>
+          <div className="ui raised segment">
+            <div className="six wide centered column">
+              <div className="ui form">
+                <div className="fields">
+                  <div className="six wide field">
+                    <TextField
+                      label="Customer Name"
+                      value={values.customerName}
+                      onChange={(customerName) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          customerName,
+                        }))
+                      }
+                    />
+                  </div>
 
-              <div className="six wide field">
-                <TextField
-                  label="Customer ID"
-                  value={values.customerID}
-                  onChange={(customerID) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      customerID,
-                    }))
-                  }
-                />
-              </div>
+                  <div className="six wide field">
+                    <TextField
+                      label="Customer ID"
+                      value={values.customerID}
+                      onChange={(customerID) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          customerID,
+                        }))
+                      }
+                    />
+                  </div>
 
-              <div className="six wide field">
-                <TextField
-                  label="Status"
-                  value={values.status}
-                  onChange={(status) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      status,
-                    }))
-                  }
-                />
+                  <div className="six wide field">
+                    <TextField
+                      label="Status"
+                      value={values.status}
+                      onChange={(status) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          status,
+                        }))
+                      }
+                    />
+                  </div>
+                </div>
+
+                <div className="fields">
+                  <div className="six wide field">
+                    <TextField
+                      label="Email"
+                      value={values.email}
+                      onChange={(email) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          email,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="Phone Number"
+                      value={values.phoneNumber}
+                      onChange={(phoneNumber) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          phoneNumber,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="Domain"
+                      value={values.domain}
+                      onChange={(domain) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          domain,
+                        }))
+                      }
+                    />
+                  </div>
+                </div>
+
+                <div className="fields">
+                  <div className="six wide field">
+                    <TextField
+                      label="Plan Type"
+                      value={values.planType}
+                      onChange={(planType) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          planType,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="Created"
+                      value={values.created}
+                      onChange={(created) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          created,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="Due Date"
+                      value={values.dueDate}
+                      onChange={(dueDate) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          dueDate,
+                        }))
+                      }
+                    />
+                  </div>
+                </div>
+
+                <div className="fields">
+                  <div className="six wide field">
+                    <TextField
+                      label="Category"
+                      value={values.category}
+                      onChange={(category) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          category,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="Description"
+                      value={values.description}
+                      onChange={(description) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          description,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="Priority"
+                      value={values.priority}
+                      onChange={(priority) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          priority,
+                        }))
+                      }
+                    />
+                  </div>
+                </div>
+
+                <div className="fields">
+                  <div className="six wide field">
+                    <TextField
+                      label="Website Specialist"
+                      value={values.websiteSpecialist}
+                      onChange={(websiteSpecialist) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          websiteSpecialist,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="Developer"
+                      value={values.developer}
+                      onChange={(developer) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          developer,
+                        }))
+                      }
+                    />
+                  </div>
+
+                  <div className="six wide field">
+                    <TextField
+                      label="QA Agent"
+                      value={values.qaAgent}
+                      onChange={(qaAgent) =>
+                        setValues((prev) => ({
+                          ...prev,
+                          qaAgent,
+                        }))
+                      }
+                    />
+                  </div>
+                </div>
+
+                <button
+                  className="ui button primary"
+                  onClick={() => updateTicket(id)}
+                >
+                  Update
+                </button>
               </div>
             </div>
-
-            <div className="fields">
-              <div className="six wide field">
-                <TextField
-                  label="Email"
-                  value={values.email}
-                  onChange={(email) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      email,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="Phone Number"
-                  value={values.phoneNumber}
-                  onChange={(phoneNumber) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      phoneNumber,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="Domain"
-                  value={values.domain}
-                  onChange={(domain) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      domain,
-                    }))
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="fields">
-              <div className="six wide field">
-                <TextField
-                  label="Plan Type"
-                  value={values.planType}
-                  onChange={(planType) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      planType,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="Created"
-                  value={values.created}
-                  onChange={(created) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      created,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="Due Date"
-                  value={values.dueDate}
-                  onChange={(dueDate) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      dueDate,
-                    }))
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="fields">
-              <div className="six wide field">
-                <TextField
-                  label="Category"
-                  value={values.category}
-                  onChange={(category) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      category,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="Description"
-                  value={values.description}
-                  onChange={(description) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      description,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="Priority"
-                  value={values.priority}
-                  onChange={(priority) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      priority,
-                    }))
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="fields">
-              <div className="six wide field">
-                <TextField
-                  label="Website Specialist"
-                  value={values.websiteSpecialist}
-                  onChange={(websiteSpecialist) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      websiteSpecialist,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="Developer"
-                  value={values.developer}
-                  onChange={(developer) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      developer,
-                    }))
-                  }
-                />
-              </div>
-
-              <div className="six wide field">
-                <TextField
-                  label="QA Agent"
-                  value={values.qaAgent}
-                  onChange={(qaAgent) =>
-                    setValues((prev) => ({
-                      ...prev,
-                      qaAgent,
-                    }))
-                  }
-                />
-              </div>
-            </div>
-
-            <button
-              className="ui button primary"
-              onClick={() => updateTicket(id)}
-            >
-              Update
-            </button>
           </div>
         </div>
       </div>
