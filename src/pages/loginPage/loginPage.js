058f68d038df4f172e1b2d6e4f05a613d392f517
leonredman <leonproart@yahoo.com>
2022-11-20 19:44:24 -0700

    added header

--- src/pages/loginPage/loginPage.js
+++ src/pages/loginPage/loginPage.js
@@ -1,17 +1,21 @@
 import React from "react";
+import LoginHeader from "../../components/login-header/loginHeader";
 import LoginForm from "../../components/loginForm/LoginForm";
 import DemoLogin from "../../components/demoLogin/DemoLogin";
 import "../../components/mainLogin/MainLoginStyles.css";
 
 const LoginPage = () => {
   return (
-    <div className="ui-container-login" id="loginBackgroundImg">
-      <div className="ui grid">
-        <div className="ui-container-login">
-          <div className="row">
-            <div className="four wide centered columm">
-              <LoginForm />
-              <DemoLogin />
+    <div>
+      <LoginHeader />
+      <div className="ui-container-login" id="loginBackgroundImg">
+        <div className="ui grid">
+          <div className="ui-container-login">
+            <div className="row">
+              <div className="four wide centered columm">
+                <LoginForm />
+                <DemoLogin />
+              </div>
             </div>
           </div>
         </div>
