df855fdfff0e6632d09fc27c788ffcce9ffc07c9
leonredman <leonproart@yahoo.com>
2023-09-08 23:08:13 -0700

    add smooth scroll

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 //import LoginForm from './components/LoginForm';
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+import "./index.css";
+ReactDOM.render(<App />, document.querySelector("#root"));
